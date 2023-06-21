a125c4c950a34847b3f8bd47c6dcd4708c720d78
OthmaneWikom <o.dghoughi@wikom.de>
2023-06-21 13:01:22 +0200

    WB-15 Notiz-Komponent

---
+++ frontend/whiteboard-app/src/components/NotizBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react' ;
+import { FaTrash } from 'react-icons/fa';
+import Note from '../types';
+import '../index.css';
+
+const NotizBox = ({noteId , title , description} : Note ) => {
+    return (
+        <div  className='whiteBoardParent_NotizBox'>
+        <div className="content_child" >
+        <div className="notizen_titel">
+            <h1>{title}  </h1>
+        </div>
+        <div className="notizen_deskription">
+            <ul>
+               
+                <li>{description}</li>
+            </ul>
+        </div>
+    </div>
+   </div>
+      )
+}
+
+export default NotizBox
--- frontend/whiteboard-app/src/components/WhiteboardPage.tsx
+++ frontend/whiteboard-app/src/components/WhiteboardPage.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import useNoteRequest from "../hooks/useNoteRequest";
+import NotizBox from "./NotizBox";
 
 const WhiteboardPage = () => {
     const {notes} = useNoteRequest();
     return  (
         <div className="whiteBoardPageParent">
+          <div className="whiteBoardParent">
             <h1>Whiteboard</h1>
             <ul>
               {
                 notes !== undefined
                ? (notes.map(note => (
-                <li key={note.noteId}>
-                  <h3>{note.title}</h3>
-                  <div>{note.description}</div>
-                </li>
+               <NotizBox  noteId= {note.noteId} 
+                key={note.noteId}
+                title= {note.title}
+                description={note.description}/>
               )))
               : (<div>Fehler beim Laden der Notizen</div>)
                }
             </ul>
+            </div>
         </div>
     );
 }
