e2406876674c340309911ccecf40cdc8955e95ce
Kouekem18 <u.kouekem@wikom.de>
2023-06-26 13:20:34 +0200

    New Header

--- frontend/whiteboard-app/src/components/WhiteboardPage.tsx
+++ frontend/whiteboard-app/src/components/WhiteboardPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import Note from "../types";
 
 const WhiteboardPage = () => {
@@ -38,48 +39,80 @@ const WhiteboardPage = () => {
     setIsFormOpen(false);
   };
 
-  return (
-    <>
-      <h1>Whiteboard</h1>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.noteId}>
-            <h3>{note.title}</h3>
-            <div>{note.description}</div>
-          </li>
-        ))}
-      </ul>
-      {!isFormOpen ? (
-        <button className="add-note-button" onClick={handleAddNoteClick}>
-          + Add Note
-        </button>
-      ) : (
-        <form className="note-form" onSubmit={handleNoteSubmit}>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={noteForm.title}
-            onChange={handleNoteChange}
-          />
-          <textarea
-            name="description"
-            placeholder="Description"
-            value={noteForm.description}
-            onChange={handleNoteChange}
-            className="note-form-textarea"
-            
-            
-          />
-          <button className="AddNote" type="submit">Add</button> 
-          <button  className="Cancel" type="button" onClick={handleCancelClick}>
-            Cancel
-          </button>
+  useEffect(() => {
+    
+    document.body.classList.toggle("dark-mode", isFormOpen);
+  }, [isFormOpen]);
 
-          </form>
-      )}
-    </>
+  const handleEditNote = () => {
+    
+  };
+
+  const handleDeleteNote = () => {
+    
+  };
+
+  return (
+    <div>
+      <header className="header">
+        <nav className="menu">
+          <ul>
+            <li>
+              <img src={require("../images/logo2.png")} alt="Logo" />
+            </li>
+            <li>
+              <button className="neueNotiz" onClick={handleAddNoteClick}> + Neue Notiz</button>
+              
+            </li>
+            <li>
+              <button className="notizbearbeiten" onClick={handleEditNote}> Notiz bearbeiten</button>
+              
+            </li>
+            <li>
+              <button className="notizlöschen" onClick={handleDeleteNote}> Notiz löschen</button>
+             
+              
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="body">
+        <div className="box">
+          <ul>
+            {notes.map((note) => (
+              <li key={note.noteId}>
+                <h3>{note.title}</h3>
+                <div>{note.description}</div>
+              </li>
+            ))}
+          </ul>
+          {isFormOpen && (
+            <form className="note-form" onSubmit={handleNoteSubmit}>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                value={noteForm.title}
+                onChange={handleNoteChange}
+              />
+              <textarea
+                name="description"
+                placeholder="Description"
+                value={noteForm.description}
+                onChange={handleNoteChange}
+                className="note-form-textarea"
+              />
+              <button className="AddNote" type="submit">Add</button>
+              <button className="Cancel" type="button" onClick={handleCancelClick}>
+                Cancel
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default WhiteboardPage;
+
