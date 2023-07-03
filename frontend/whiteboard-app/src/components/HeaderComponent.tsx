ac3131d103267ffd917dfdcd1047a1358593db71
Kouekem18 <u.kouekem@wikom.de>
2023-07-03 14:46:09 +0200

    Update HeaderComponent

---
+++ frontend/whiteboard-app/src/components/HeaderComponent.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaPlus, FaPen, FaTrash } from 'react-icons/fa';
+
+interface HeaderComponentProps {
+  handleAddNoteClick: () => void;
+  handleEditNote: () => void;
+  handleDeleteNote: () => void;
+}
+
+const HeaderComponent: React.FC<HeaderComponentProps> = ({
+  handleAddNoteClick,
+  handleEditNote,
+  handleDeleteNote
+}) => {
+  return (
+    <header className="header">
+      <nav className="menu">
+        <ul>
+          <li>
+            <img src={require("../images/logo2.png")} alt="Logo" />
+          </li>
+          <li>
+            <button className="neueNotiz" onClick={handleAddNoteClick}>
+              <FaPlus /> Neue Notiz
+            </button>
+          </li>
+          <li>
+            <button className="notizbearbeiten" onClick={handleEditNote}>
+              <FaPen /> Notiz bearbeiten
+            </button>
+          </li>
+          <li>
+            <button className="notizloeschen" onClick={handleDeleteNote}>
+              <FaTrash /> Notiz löschen
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderComponent;
--- frontend/whiteboard-app/src/components/WhiteboardPage.tsx
+++ frontend/whiteboard-app/src/components/WhiteboardPage.tsx
@@ -1,5 +1,8 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import Note from "../types";
+import HeaderComponent from "./HeaderComponent";
+
+
 
 const WhiteboardPage = () => {
   const [noteForm, setNoteForm] = useState({
@@ -38,47 +41,60 @@ const WhiteboardPage = () => {
     setIsFormOpen(false);
   };
 
-  return (
-    <>
-      <h1>Whiteboard</h1>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.noteId}>
-            <h3>{note.title}</h3>
-            <div>{note.description}</div>
-          </li>
-        ))}
-      </ul>
-      {!isFormOpen ? (
-        <button className="add-note-button" onClick={handleAddNoteClick}>
-          + Add Note
-        </button>
-      ) : (
-        <form className="note-form" onSubmit={handleNoteSubmit}>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={noteForm.title}
-            onChange={handleNoteChange}
-          />
-          <textarea
-            name="description"
-            placeholder="Description"
-            value={noteForm.description}
-            onChange={handleNoteChange}
-            className="note-form-textarea"
-            
-            
-          />
-          <button className="AddNote" type="submit">Add</button> 
-          <button  className="Cancel" type="button" onClick={handleCancelClick}>
-            Cancel
-          </button>
+  const handleEditNote = () => {
+    
+  };
 
-          </form>
-      )}
-    </>
+  const handleDeleteNote = () => {
+   
+  };
+
+  return (
+    <div>
+      <HeaderComponent
+        handleAddNoteClick={handleAddNoteClick}
+        handleEditNote={handleEditNote}
+        handleDeleteNote={handleDeleteNote}
+      />
+      <div className="body">
+        <div className="box">
+          <ul>
+            {notes.map((note) => (
+              <li key={note.noteId}>
+                <h3>{note.title}</h3>
+                <div>{note.description}</div>
+              </li>
+            ))}
+          </ul>
+          {isFormOpen && (
+            <form className="note-form" onSubmit={handleNoteSubmit}>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                value={noteForm.title}
+                onChange={handleNoteChange}
+              />
+              <textarea
+                name="description"
+                placeholder="Description"
+                value={noteForm.description}
+                onChange={handleNoteChange}
+                className="note-form-textarea"
+              />
+              <div className="button-container">
+                <button className="AddNote" type="submit">
+                  Add
+                </button>
+                <button className="Cancel" type="button" onClick={handleCancelClick}>
+                  Cancel
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
